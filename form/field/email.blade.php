3ca7b016f6dc3c385c1e47fd429f5e56f71403b6
AO™ <anthony@osawere.com>
2024-11-17 03:11:39 +0100

    Create email.blade.php

---
+++ form/field/email.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'id' => 'email',
+    'name' => 'email',
+    'title' => 'email address',
+    'value' => '',
+    'disable' => false,
+    'obind' => true,
+    'ochange' => false,
+    'validateJS' => true,
+    'btnID' => 'sendBtn',
+    'css' => '',
+
+    'placeholder' => 'email address',
+    'ocomplete' => true,
+])
+<x-veo.form.input.field.text :id="$id" :name="$name" :title="$title" :placeholder="$placeholder" :disable=$disable :obind=$obind :ochange=$ochange :validateJS=$validateJS :btnID=$btnID :value=$value :ocomplete=$ocomplete type="email" {{ $attributes }} />
