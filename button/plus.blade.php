751a8e3a8b41857c93bebe1c0acc93626db8129d
AO™ <anthony@osawere.com>
2024-11-18 08:10:34 +0100

    Update plus.blade.php

--- button/plus.blade.php
+++ button/plus.blade.php
@@ -1,22 +1,6 @@
 @props(['label', 'type' => 'submit', 'scheme' => 'primary', 'id' => null, 'title' => null])
 @php
-	if ($type === 'submit') {
-	    $type = in_array($label, ['reset', 'clear'], true) ? 'reset' : $type;
-	}
-	if ($type === 'reset') {
-	    if (empty($label)) {
-	        $label = 'clear';
-	    }
-	    $scheme = StringX::swap($scheme, 'primary', 'gray');
-	}
-	$scheme = $scheme === 'gray' ? 'gray-100' : $scheme;
-	$scheme = $scheme === 'outline-gray' ? 'outline-gray-200' : $scheme;
-	if (empty($id) && !empty($label) && $slot->isEmpty()) {
-	    $id = $label;
-	}
-	if (!empty($title)) {
-	    $title = ucwords($title);
-	}
+	FormX::set()->button(label: $label, id: $id, title: $title, type: $type, scheme: $scheme);
 @endphp
 
 <button id='{{ $id }}' {{ $attributes->merge(['type' => $type, 'class' => 'btn btn-' . $scheme . ' text-uppercase mx-2']) }} title="{{ $title }}">
