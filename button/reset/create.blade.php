f952d2e5f7509351d0a487ef54a72f901bed3e05
AO™ <anthony@osawere.com>
2024-11-18 08:10:38 +0100

    Update create.blade.php

--- button/reset/create.blade.php
+++ button/reset/create.blade.php
@@ -1,7 +1,6 @@
-@props(['label' => 'Reset', 'moduleX' => ''])
+@props(['label' => 'reset', 'moduleX' => ''])
 @php
-	if (!empty($moduleX)) {
-	    $label .= ' ' . $moduleX;
-	}
+	FormX::set()->button(label: $label, module: $moduleX);
 @endphp
-<button wire:loading.attr="disabled" wire:target="save" onclick="jsHideError('actionBtn');" type="reset" class="btn btn-block btn-outline-gray-300 mx-2">{{ $label }}</button>
+
+<button wire:loading.attr="disabled" wire:target="save" onclick="jsErrorHide('actionBtn');" type="reset" class="btn btn-block btn-outline-gray-300 mx-2">{{ $label }}</button>
