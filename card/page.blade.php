9709da8f069e55766187a5da7622a026400280c8
AO™ <anthony@osawere.com>
2024-11-18 12:08:24 +0100

    Update page.blade.php

--- card/page.blade.php
+++ card/page.blade.php
@@ -1,6 +1,9 @@
-
-<div class="card border-0 shadow mb-4">
-	<div class="card-body">
-		{{ $slot }}
+<div class="row">
+	<div class="col-12 mb-4">
+		<div class="card border-0 shadow mb-4">
+			<div class="card-body">
+				{{ $slot }}
+			</div>
+		</div>
 	</div>
 </div>
