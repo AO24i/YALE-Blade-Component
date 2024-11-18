aefd6074e3926226c3c1b641270a9a81b7336e8e
AO™ <anthony@osawere.com>
2024-11-18 08:11:10 +0100

    Update auth.blade.php

--- layout/auth.blade.php
+++ layout/auth.blade.php
@@ -1,3 +1,4 @@
+
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
 	<div class="w-full sm:max-w-md mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg">
 		<div class="flex items-center justify-center mb-6">
