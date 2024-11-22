76cd8464db322556aa90673c8f86aeb0144228b0
AO™ <anthony@osawere.com>
2024-11-22 11:08:19 +0100

    Update js.blade.php

--- plug/js.blade.php
+++ plug/js.blade.php
@@ -1,3 +1,16 @@
-@props(['src', 'tag' => 'js'])
+@props(['src', 'tag' => 'js', 'async' => false, 'defer' => false])
+@php
+	$attributes = collect();
 
-<script src="{{ AssetX::href($src, $tag)}}"></script>
+	if ($async) {
+	    $attributes->push('async');
+	}
+
+	if ($defer) {
+	    $attributes->push('defer');
+	}
+
+	$attributeString = $attributes->implode(' ');
+@endphp
+
+<script {{ $attributeString }} src="{{ AssetX::href($src, $tag) }}"></script>
