d17e2c32e97622c6bdcc8865c3b556cf0db51c91
AO™ <anthony@osawere.com>
2024-11-27 12:01:33 +0100

    Create row-image.blade.php

---
+++ table/row-image.blade.php
@@ -0,0 +1,4 @@
+@props(['src' => '', 'alt' => '', 'class' => ''])
+<td {{ $attributes }}>
+	<img src="{{ $src }}" alt="{{ $alt }}" title="{{ $alt }}" {{ $attributes->merge(['class' => $class]) }} />
+</td>
